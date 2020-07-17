dd63da45d6221e463ede4b67217601370fbb84bc
christopherRyan01 <chrisg450@gmail.com>
2020-07-17 14:48:30 -0700

    Refix

--- js/end.js
+++ js/end.js
@@ -14,26 +14,26 @@ username.addEventListener("keyup", () => {
     saveScoreBtn.disabled = !username.value;
 });
 
-saveHighScore = e => {
-    console.log("clicked the save button")
-    e.preventDefault();
+// saveHighScore = e => {
+//     console.log("clicked the save button")
+//     e.preventDefault();
 
-    const score = {
-        score: mostRecentScore,
-        name: username.value,
-    };
+//     const score = {
+//         score: mostRecentScore,
+//         name: username.value,
+//     };
 
-    highScores.push(score);
+//     highScores.push(score);
 
-    highScores.sort( (a,b) => {
-        return b.score - a.score
-    });
+//     highScores.sort( (a,b) => {
+//         return b.score - a.score
+//     });
 
-    highScores.splice(10);
+//     highScores.splice(10);
     
-    // localStorage.setItem("highScores", JSON.stringify(highScores));
-    // window.location.assign("index.html")
-};
+//     localStorage.setItem("highScores", JSON.stringify(highScores));
+//     window.location.assign("index.html")
+// };
 
 
 let sound = new Audio('audio/boop.wav');
